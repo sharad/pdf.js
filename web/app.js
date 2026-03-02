352d954b0cfa94a3d08e036b2a20686a4d496c6f
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-03-03 02:16:03 +0530

    autosave web/app.js after save

--- web/app.js
+++ web/app.js
@@ -2195,26 +2195,7 @@ const PDFViewerApplication = {
     const exportBtn = document.getElementById("exportSelectedButton");
 
     if (exportBtn) {
-      exportBtn.addEventListener("click", () => {
-        const thumbnailViewer = PDFViewerApplication.pdfThumbnailViewer;
-
-        if (!thumbnailViewer) {
-          console.error("Thumbnail viewer not ready");
-          return;
-        }
-
-        const pagesMapper = thumbnailViewer._pagesMapper;
-
-        if (!pagesMapper) {
-          console.error("PagesMapper not available");
-          return;
-        }
-
-        PDFViewerApplication.eventBus.dispatch("savepageseditedpdf", {
-          source: PDFViewerApplication,
-          data: pagesMapper.getPageMappingForSaving(),
-        });
-      }
+      exportBtn.addEventListener("click", () =>
 
 
 
