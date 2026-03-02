ea2b63511ace3197d44b84109419d213af527ad7
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-03-03 01:51:40 +0530

    autosave web/app.js after save

--- web/app.js
+++ web/app.js
@@ -2504,31 +2504,10 @@ PDFViewerApplication.initializedPromise.then(() => {
     return;
   }
 
-  // btn.addEventListener("click", () => {
-  //   console.log("EXPORT CLICKED");
+  btn.addEventListener("click", () => {
+    console.log("EXPORT CLICKED");
 
-  //   const viewer =
-  //         PDFViewerApplication.pdfThumbnailViewer;
-
-  //   const selected =
-  //         PDFViewerApplication.pdfThumbnailViewer.getSelectedPages() || [];
-  //         // viewer?._getSelectedPages?.() || [];
-
-  //   console.log("Selected:", selected);
-
-  //   if (!selected.length) {
-  //     alert("No pages selected");
-  //     return;
-  //   }
-
-  //   PDFViewerApplication.eventBus.dispatch(
-  //     "savepageseditedpdf",
-  //     {
-  //       source: PDFViewerApplication,
-  //       data: { pageNumbers: selected }
-  //     }
-  //   );
-  // });
+  });
 });
 
 if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
